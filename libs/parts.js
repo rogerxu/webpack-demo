69bd1ac95564fde7be5750db8e9ed51d04ad28ca
Roger Xu <roger.xu.2007@gmail.com>
2016-10-25 16:15:01 +0800

    Add minification

--- libs/parts.js
+++ libs/parts.js
@@ -40,3 +40,29 @@ exports.setupCSS = function(paths) {
     }
   }
 };
+
+exports.minify = function() {
+  return {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+
+        comments: false,
+
+        compress: {
+          warnings: false,
+
+          drop_console: true
+        },
+
+        mangle: {
+          except: ['$'],
+
+          screw_ie8: true,
+
+          keep_fnames: true
+        }
+      })
+    ]
+  };
+};
