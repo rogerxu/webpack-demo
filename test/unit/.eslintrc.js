e2762bf114bc3d4f951ae8a0b1578a468d0ac925
Roger Xu <roger.xu.2007@gmail.com>
2017-04-04 17:06:56 +0800

    Add chai

--- test/unit/.eslintrc.js
+++ test/unit/.eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
   "globals": {
   },
   "rules": {
+    "dot-notation": ["error", {"allowKeywords": true}],
   }
 };
--- test/unit/util/util.js
+++ test/unit/util/util.js
@@ -1,8 +1,8 @@
-import assert from 'assert';
+import { expect } from 'chai';
 import { isProd } from '../../../app/util/util';
 
 describe('util', () => {
   it('should not be in production mode', () => {
-    assert.strictEqual(isProd, false);
+    expect(isProd).to.be.false;
   });
 });
